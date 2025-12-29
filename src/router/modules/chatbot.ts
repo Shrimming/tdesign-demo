2fe129a1154db7c4a125b2cac3e4488134fa77ad
咱家最喜欢了 <16518076+ailiailiai@user.noreply.gitee.com>
2025-12-29 10:18:19 +0800

    添加chatbot目录

---
+++ src/router/modules/chatbot.ts
@@ -0,0 +1,34 @@
+import Layout from '@/layouts/index.vue';
+
+export default [
+  {
+    path: '/user',
+    name: 'user',
+    component: Layout,
+    redirect: '/user/index',
+    meta: { title: { zh_CN: '个人中心', en_US: 'User Center' }, icon: 'user-circle' },
+    children: [
+      {
+        path: 'index',
+        name: 'UserIndex',
+        component: () => import('@/pages/chatbot/index.vue'),
+        meta: { title: { zh_CN: '个人中心', en_US: 'User Center' } },
+      },
+    ],
+  },
+  {
+    path: '/loginRedirect',
+    name: 'loginRedirect',
+    redirect: '/login',
+    meta: { title: { zh_CN: '登录页', en_US: 'Login' }, icon: shallowRef(LogoutIcon) },
+    component: () => import('@/layouts/blank.vue'),
+    children: [
+      {
+        path: 'index',
+        redirect: '/login',
+        component: () => import('@/layouts/blank.vue'),
+        meta: { title: { zh_CN: '登录页', en_US: 'Login' } },
+      },
+    ],
+  },
+];
